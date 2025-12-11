e4cad4d3f993277e4b1dcc8e75324866a4d42688
Mohammadali Khalifa <Mohammadali_khalifa@student.uml.edu>
2025-12-11 14:35:35 -0500

    Update setup.py

--- src/abot_01_pkg/setup.py
+++ src/abot_01_pkg/setup.py
@@ -1,40 +1,31 @@
 from setuptools import find_packages, setup
-
-package_name = 'moali_pkg'
-
+package_name = 'abot_01_pkg'
 setup(
-    name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
-    data_files=[
-        ('share/ament_index/resource_index/packages',
+   name=package_name,
+   version='0.0.0',
+   packages=find_packages(exclude=['test']),
+   data_files=[
+       ('share/ament_index/resource_index/packages',
         ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name, ['launch/move_square_launch.py']),
-        ('share/' + package_name, ['launch/square_distance_launch.py']),
-        ('share/' + package_name, ['launch/convert_launch.py']),
-        ('share/' + package_name, ['launch/FSM_launch.py']),
-    ],
-    
-    install_requires=['setuptools'],
-    zip_safe=True,
-    maintainer='moham',
-    maintainer_email='moham@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
-    entry_points={
-        'console_scripts': [
-            'moves_square = moali_pkg.move_square:main',
-            'square_distance = moali_pkg.square_distance:main',
-            'convert = moali_pkg.convert:main',
-            'FSM = moali_pkg.FSM:main',
-            'Mode = moali_pkg.Mode:main',
-            'Estop = moali_pkg.Estop:main',
-        ],
-    },
+       ('share/' + package_name,
+        ['package.xml']),
+       # install launch file(s)
+       ('share/' + package_name + '/launch', [
+           'launch/follow_ball_launch.py',
+       ]),
+   ],
+   install_requires=['setuptools'],
+   zip_safe=True,
+   maintainer='student',
+   maintainer_email='student@example.com',
+   description='FSM + PID controller to make abot-01 follow a blue pingpong ball.',
+   license='TODO',
+   tests_require=['pytest'],
+   entry_points={
+       'console_scripts': [
+           'fsm = abot_01_pkg.FSM:main',
+           'mode = abot_01_pkg.Mode:main',
+           'follow_ball_controller = abot_01_pkg.follow_ball_controller:main',
+       ],
+   },
 )
