31ce6910b3d601edcf817ac42c5ed2e4b087253f
Mohammadali Khalifa <Mohammadali_khalifa@student.uml.edu>
2025-12-11 14:45:42 -0500

    Update follow_ball_launch.py

--- src/abot_01_pkg/launch/follow_ball_launch.py
+++ src/abot_01_pkg/launch/follow_ball_launch.py
@@ -2,36 +2,30 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 def generate_launch_description():
    return LaunchDescription([
-       # Color filter from Lab 3.1 (make sure target_color == 'blue')
        Node(
-           package='color_filter_pkg',
+           package='abot_01_pkg',
            executable='filter',
            name='color_filter',
-           parameters=[{'target_color': 'blue'}],
            output='screen'
        ),
-       # Image info from Lab 3.2
        Node(
-           package='color_filter_pkg',
+           package='abot_01_pkg',
            executable='image_info',
            name='image_info',
            output='screen'
        ),
-       # PID controller that turns image_info into /auto/cmd_vel
        Node(
            package='abot_01_pkg',
            executable='follow_ball_controller',
            name='follow_ball_controller',
            output='screen'
        ),
-       # FSM that muxes teleop vs auto into /abot/cmd_vel
        Node(
            package='abot_01_pkg',
            executable='fsm',
            name='fsm',
            output='screen'
        ),
-       # Mode node for fast mode switching (t/a/e)
        Node(
            package='abot_01_pkg',
            executable='mode',
