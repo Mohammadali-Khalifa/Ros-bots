3af41b4ceea239b2e62059ea8513c5f47c24c2c4
Mohammadali Khalifa <Mohammadali_khalifa@student.uml.edu>
2025-12-13 08:13:27 -0500

    Update filter.py

--- src/abot_01_pkg/abot_01_pkg/filter.py
+++ src/abot_01_pkg/abot_01_pkg/filter.py
@@ -9,23 +9,24 @@ class ColorFilter(Node):
     def __init__(self):
         super().__init__('color_filter')
         self.bridge = CvBridge()
-        self.sub = self.create_subscription(Image, 'camera/image_raw', self.filter, 10) #subscribes to image_filter used by color_filter
+        self.sub = self.create_subscription(Image, '/camera/image_raw', self.filter, 10) #subscribes to image_filter used by color_filter
         self.pub = self.create_publisher(Image, '/image_filtered', 10) #publishes to image_filter used by color_filter
 
     def filter(self, image_message): # convert ROS image to OpenCV
-        cv_image = self.bridge.imgmsg_to_cv2(image_message, desired_encoding='bgr8')
+        bgr = self.bridge.imgmsg_to_cv2(image_message, desired_encoding='bgr8')
         hsv = cv2.cvtColor(bgr, cv2.COLOR_BGR2HSV)
         mask = cv2.inRange(hsv, (95, 120, 60), (130, 255, 255))
-       
+
 
         #lines 18 to 29 basicly do the filtering and changing the HSV numbers of low and high can filter more or less (creates a bit mask for the color)
 
-        
+
         kernel = np.ones((7,7), np.uint8)
         mask = cv2.erode(mask, kernel, iterations=1)
         mask = cv2.dilate(mask, kernel, iterations=2)
-        result = cv2.bitwise_and(img, img, mask=mask)
-        ros_img = self.bridge.cv2_to_imgmsg(result, "mono8")
+
+        # publish the MASK (mono8). This is what image_info expects.
+        ros_img = self.bridge.cv2_to_imgmsg(mask, "mono8")
         self.pub.publish(ros_img)
 
         #lines 34 to 39 are form slides 24-25 of computer vision and then it publshes the images
