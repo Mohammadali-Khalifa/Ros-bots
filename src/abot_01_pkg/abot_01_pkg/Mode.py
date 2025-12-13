57704711a81e1550c4d43601f34be8b5835fc2f1
Mohammadali Khalifa <Mohammadali_khalifa@student.uml.edu>
2025-12-13 18:09:19 -0500

    Update Mode.py

--- src/abot_01_pkg/abot_01_pkg/Mode.py
+++ src/abot_01_pkg/abot_01_pkg/Mode.py
@@ -9,17 +9,17 @@ class Mode(Node):
         self.pub = self.create_publisher(String, 'mode', 10)   # publishes a string which is the mode
 
     def mode_select(self):
-        self.get_logger().info('Type: auto, teleop, ostop, estop')    # prints txt: Type: auto, teleop, ostop, estop
+        self.get_logger().info('Type: A for auto, T for teleop, O for ostop, E for estop')    # prints txt: Type: auto, teleop, ostop, estop
         while rclpy.ok():
             user_message = input().strip().lower()                    # saves user input into user_message
 
-            if user_message == 'auto':                                # checks if user put in auto and then sends Autonomous to FSM
+            if user_message == 'A':                                # checks if user put in auto and then sends Autonomous to FSM
                 self.pub.publish(String(data='a'))
-            elif user_message == 'teleop':
+            elif user_message == 'T':
                 self.pub.publish(String(data='t'))                    # checks if user put in teleop and then sends teleoperated to FSM
-            elif user_message == 'ostop':
+            elif user_message == 'O:
                 self.get_logger().info('ostop not implemented in FSM yet (use estop or add a mode)')  # keeps your option visible
-            elif user_message == 'estop':
+            elif user_message == 'E':
                 self.pub.publish(String(data='e'))                    # checks if user put in estop and then sends ESTOP to FSM
             else:
                 self.get_logger().info('Type: auto, teleop, ostop, estop')
