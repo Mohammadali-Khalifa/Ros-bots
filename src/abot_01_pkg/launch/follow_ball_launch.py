3c374f21ba0ce1ec8e8890b89b9d6bc108e4b8bc
Mohammadali Khalifa <Mohammadali_khalifa@student.uml.edu>
2025-12-13 19:16:49 -0500

    Update follow_ball_launch.py

--- src/abot_01_pkg/launch/follow_ball_launch.py
+++ src/abot_01_pkg/launch/follow_ball_launch.py
@@ -7,7 +7,7 @@ def generate_launch_description():
            package='abot_01_pkg',
            executable='filter',
            name='color_filter',
-           output='screen',
+           output='screen'
        ),
        Node(
            package='abot_01_pkg',
@@ -15,34 +15,16 @@ def generate_launch_description():
            name='image_info',
            output='screen'
        ),
-      Node(
-          package='abot_01_pkg',
-          executable='follow_ball_controller',
-          name='follow_ball_controller',
-          output='screen',
-          parameters=[
-              {'focal_px': 664.0},
-              {'target_distance_m': 0.15},
-              {'ball_diameter_m': 0.04},
-              {'frame_width': 640.0},
-          ]
-      ),
+       Node(
+           package='abot_01_pkg',
+           executable='follow_ball_controller',
+           name='follow_ball_controller',
+           output='screen'
+       ),
        Node(
            package='abot_01_pkg',
            executable='fsm',
            name='fsm',
            output='screen'
        ),
-       Node(
-           package='abot_01_pkg',
-           executable='mode',
-           name='mode',
-           output='screen'
-       ),
-      Node(
-          package='camera_ros',
-          executable='camera_node',
-          name='camera',
-          output='screen'
-      ),
    ])
