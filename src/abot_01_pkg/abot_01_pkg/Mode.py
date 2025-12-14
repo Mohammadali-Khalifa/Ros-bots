a5871c4546e3bb77ff486cf94308dacb58473054
Mohammadali Khalifa <Mohammadali_khalifa@student.uml.edu>
2025-12-13 20:09:40 -0500

    Update Mode.py

--- src/abot_01_pkg/abot_01_pkg/Mode.py
+++ src/abot_01_pkg/abot_01_pkg/Mode.py
@@ -2,18 +2,15 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
 from std_msgs.msg import Bool
-
 class Mode(Node):
     def __init__(self):
         super().__init__('Mode_node')
         self.pub = self.create_publisher(String, 'mode', 10)   # publishes a string which is the mode
-
     def mode_select(self):
-        self.get_logger().info('Type: A for auto, T for teleop, E for estop')    # prints txt: Type: auto, teleop, ostop, estop
+        self.get_logger().info('Type: A for auto, T for teleop,E for estop')    # prints txt: Type: auto, teleop, ostop, estop
         while rclpy.ok():
             user_message = input().strip().lower()                    # saves user input into user_message
-
-            if user_message == 'a':                                   # checks if user put in auto and then sends Autonomous to FSM
+            if user_message == 'a':                                # checks if user put in auto and then sends Autonomous to FSM
                 self.pub.publish(String(data='a'))
             elif user_message == 't':
                 self.pub.publish(String(data='t'))                    # checks if user put in teleop and then sends teleoperated to FSM
@@ -21,13 +18,11 @@ class Mode(Node):
                 self.pub.publish(String(data='e'))                    # checks if user put in estop and then sends ESTOP to FSM
             else:
                 self.get_logger().info('Type: A for auto, T for teleop, E for estop')
-
 def main(args=None):
     rclpy.init(args=args)
     node = Mode()
-    node.mode_select()   # run your input loop
+    node.mode_select() 
     node.destroy_node()
     rclpy.shutdown()
-
 if __name__ == '__main__':
     main()
