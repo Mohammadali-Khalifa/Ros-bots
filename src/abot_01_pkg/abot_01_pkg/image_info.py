bea9d2ed8f45aa9a75ab4a9c08aaee7319b3a9e2
Mohammadali Khalifa <Mohammadali_khalifa@student.uml.edu>
2025-12-13 22:40:37 -0500

    Update image_info.py

--- src/abot_01_pkg/abot_01_pkg/image_info.py
+++ src/abot_01_pkg/abot_01_pkg/image_info.py
@@ -11,24 +11,16 @@ class ImageInfo(Node):
         super().__init__('image_info')
 
         self.bridge = CvBridge()  # converts ROS images to OpenCV
-
-        # subscribes to image_filtered (mono mask: white=ball, black=background)
-        self.sub = self.create_subscription(Image, 'image_filtered', self.image_cb, 10)
-
-        # publishes [center_px, width_px]
-        self.pub = self.create_publisher(Int32MultiArray, 'image_info', 10)
+        self.sub = self.create_subscription(Image, '/image_filtered', self.image_cb, 10)
+        self.sub = self.create_subscription(Image, '/image_filtered', self.image_cb, 10)
+        self.pub = self.create_publisher(Int32MultiArray, '/image_info', 10)
 
     def image_cb(self, msg: Image):
-        # convert ROS mono8 image -> OpenCV mask
         mask = self.bridge.imgmsg_to_cv2(msg, desired_encoding='mono8')
-
-        # find contours on the mask
         contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
         center_px = -1
         width_px = 0
-
-        # if we found something, take the biggest blob (assume ball)
         if contours:
             largest = max(contours, key=cv2.contourArea)
             x, y, w, h = cv2.boundingRect(largest)
