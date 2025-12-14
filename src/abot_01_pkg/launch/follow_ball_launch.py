1220fc9144bc275774cfa5435c16619684212cb0
Mohammadali Khalifa <Mohammadali_khalifa@student.uml.edu>
2025-12-13 20:11:31 -0500

    Update follow_ball_launch.py

--- src/abot_01_pkg/launch/follow_ball_launch.py
+++ src/abot_01_pkg/launch/follow_ball_launch.py
@@ -15,12 +15,18 @@ def generate_launch_description():
            name='image_info',
            output='screen'
        ),
-       Node(
-           package='abot_01_pkg',
-           executable='follow_ball_controller',
-           name='follow_ball_controller',
-           output='screen'
-       ),
+      Node(
+          package='abot_01_pkg',
+          executable='follow_ball_controller',
+          name='follow_ball_controller',
+          output='screen',
+          parameters=[{
+              'focal_px': 664.0,
+              'target_distance_m': 0.15,
+              'ball_diameter_m': 0.04,
+              'frame_width': 640.0
+          }]
+      ),
        Node(
            package='abot_01_pkg',
            executable='fsm',
