5e5bca82e07bed6abeef7ab4e569c3bbc1186937
Mohammadali Khalifa <Mohammadali_khalifa@student.uml.edu>
2025-12-13 22:34:23 -0500

    Update Mode.py

--- src/abot_01_pkg/abot_01_pkg/Mode.py
+++ src/abot_01_pkg/abot_01_pkg/Mode.py
@@ -6,23 +6,20 @@ from std_msgs.msg import Bool
 class Mode(Node):
     def __init__(self):
         super().__init__('Mode_node')
-        self.pub = self.create_publisher(String, 'mode', 10)   # publishes a string which is the mode
+        self.pub = self.create_publisher(String, '/mode', 10)   # publishes a string which is the mode
 
     def mode_select(self):
-        self.get_logger().info('Type: A for auto, T for teleop, O for ostop, E for estop')    # prints txt
+        self.get_logger().info('Type: A for auto, T for teleop, E for estop')    # prints txt
         while rclpy.ok():
             user_message = input().strip().lower()  # saves user input into user_message
-
             if user_message == 'a':  # auto
                 self.pub.publish(String(data='a'))
             elif user_message == 't':  # teleop
                 self.pub.publish(String(data='t'))
-            elif user_message == 'o':  # ostop (not in FSM)
-                self.get_logger().info('ostop not implemented in FSM yet (use estop or add a mode)')
             elif user_message == 'e':  # estop
                 self.pub.publish(String(data='e'))
             else:
-                self.get_logger().info('Type: A for auto, T for teleop, O for ostop, E for estop')
+                self.get_logger().info('Type: A for auto, T for teleop, E for estop')
 
 def main(args=None):
     rclpy.init(args=args)
