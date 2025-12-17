86cfaf351a4534d87147d229338f9fb8e5b5db35
cmd822 <christina_dennis@student.uml.edu>
2025-12-16 20:11:18 -0500

    Adding comments
    Added comments for better code clarity and understanding.

--- src/Final_pkg/Final_pkg/gripper_serial_node.py
+++ src/Final_pkg/Final_pkg/gripper_serial_node.py
@@ -4,28 +4,31 @@ from std_msgs.msg import String
 import serial
 import time
 
+#node for controlling gripper using ESP32
 class GripperSerial(Node):
     def __init__(self):
         super().__init__('gripper_serial')
         
-        self.port = '/dev/ttyACM0'
-        self.baud = 115200
+        self.port = '/dev/ttyACM0' #port
+        self.baud = 115200 #baud rate
 
+        #attempt to open serial connection
         try:
             self.ser = serial.Serial(self.port, self.baud, timeout=1)
             time.sleep(2)
             self.get_logger().info(f'Connected to ESP32 on {self.port}')
-        except Exception as e:
+        except Exception as e: #handle connection failure
             self.get_logger().error(f'Failed to open serial port: {e}')
             self.ser = None
 
-        self.sub = self.create_subscription(
+        self.sub = self.create_subscription( #subscribe to gripper command topic
             String,
             'gripper_cmd',
             self.cmd_callback,
             10
         )
 
+    #callback for gripper commands entered to send to serial port
     def cmd_callback(self, msg):
         if self.ser:
             cmd = msg.data.strip()
